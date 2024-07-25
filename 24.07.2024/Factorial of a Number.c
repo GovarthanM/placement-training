0830a50b884b9cbe0c787ed2e45285120b3e73db
GOVARTHAN M <139614860+GovarthanM@users.noreply.github.com>
2024-07-25 20:15:24 +0530

    Create Factorial of a Number.c

---
+++ 24.07.2024/Factorial of a Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n < 0)
+        printf("Error");
+    else {
+        for(i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("%llu\n",factorial);
+    }
+    return 0;
+}
